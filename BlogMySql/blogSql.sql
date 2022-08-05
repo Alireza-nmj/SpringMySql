85b3e2b4b876fda56d3b8cc91aea3fdb3bcd378c
Alireza-nmj <74626752+Alireza-nmj@users.noreply.github.com>
2022-08-05 19:01:39 +0430

    Add files via upload

---
+++ BlogMySql/blogSql.sql
@@ -0,0 +1,10 @@
+SELECT id,title,content
+FROM `restapi`.`blog`;
+INSERT INTO `restapi`.`blog` (id, title, content)
+VALUES (1,"myBlog","welcome to my blog");
+INSERT INTO `restapi`.`blog` (id, title, content)
+VALUES (2,"TedBlog","welcome to ted blog");
+INSERT INTO `restapi`.`blog` (id, title, content)
+VALUES (3,"BarneyBlog","welcome to barney blog");
+INSERT INTO `restapi`.`blog` (id, title, content)
+VALUES (4,"TBagBlog","welcome to TBag blog");
